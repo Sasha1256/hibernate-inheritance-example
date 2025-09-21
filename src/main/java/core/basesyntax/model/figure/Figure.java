6bfb412468936ef301a5db47998999e0501fd7e7
Sasha Koval <kovalsana40@gmail.com>
2025-09-21 20:44:04 +0300

    i want to check with AI

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,6 +1,9 @@
 package core.basesyntax.model.figure;
 
-import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class Figure {
